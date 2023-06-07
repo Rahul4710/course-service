c4b1e3f7490d255a42ccf00c672322437cf18844
Rahul <rahul09singh08@gmail.com>
2023-06-08 00:52:40 +0530

    first change

---
+++ src/main/java/com/thenext/app/SpringbootRsJpaApplication.java
@@ -0,0 +1,13 @@
+package com.thenext.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRsJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRsJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thenext/app/SpringbootRsJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thenext.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRsJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
